addc4f9ca4beaf1814dacb52aec8365ca96eda98
SirNiclis <nicolas_mayorga@yahoo.es>
2020-09-15 10:26:01 -0500

    Se añadió lac lase CustomerMapper

--- src/main/java/co/edu/usbcali/demo/rest/CustomerController.java
+++ src/main/java/co/edu/usbcali/demo/rest/CustomerController.java
@@ -1,17 +1,23 @@
 package co.edu.usbcali.demo.rest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.usbcali.demo.domain.Customer;
 import co.edu.usbcali.demo.dto.CustomerDTO;
+import co.edu.usbcali.demo.mapper.CustomerMapper;
 import co.edu.usbcali.demo.repository.CustomerRepository;
 
 @RestController
@@ -23,30 +29,56 @@ public class CustomerController {
 	@Autowired
 	CustomerRepository customerRepository;
 	
+	@Autowired
+	CustomerMapper customerMapper;
+	
+	@PostMapping("/save")
+	public ResponseEntity<?> save(@RequestBody CustomerDTO customerDTO){
+		try {
+			Customer customer = customerMapper.toCustomer(customerDTO);
+			customer = customerRepository.save(customer);
+			customerDTO = customerMapper.toCustomerDTO(customer);
+			return ResponseEntity.ok().body(customerDTO);
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@GetMapping("/findAll")
+	public ResponseEntity<?> findAll(){
+		try {
+			//Lista de customers
+			List<Customer> customers = customerRepository.findAll();
+			//Arreglo de DTOs
+			List<CustomerDTO> customerDTOs = customerMapper.toCustomersDTO(customers);
+			
+			return ResponseEntity.ok().body(customerDTOs);
+			
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
 	@GetMapping("/findById/{email}")
-	public CustomerDTO findById(@PathVariable("email") String email) {
+	public ResponseEntity<?> findById(@PathVariable("email") String email) {
 		
 		try {
 			Optional<Customer> customerOptional = customerRepository.findById(email);
 			if (customerOptional.isPresent()==false) {
-				return null;
+				return ResponseEntity.ok().body("Customer not found.");
 			}
 			
 			Customer customer = customerOptional.get();
-			CustomerDTO customerDTO = new CustomerDTO();
-			customerDTO.setAddress(customer.getAddress());
-			customerDTO.setEmail(customer.getEmail());
-			customerDTO.setEnable(customer.getEnable());
-			customerDTO.setName(customer.getName());
-			customerDTO.setPhone(customer.getPhone());
-			customerDTO.setToken(customer.getToken());
+			CustomerDTO customerDTO = customerMapper.toCustomerDTO(customer);
 			
-			return customerDTO;
+			return ResponseEntity.ok().body(customerDTO);
 			
 			
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
-			return null;
+			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 		
 	}
